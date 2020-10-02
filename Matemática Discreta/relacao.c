5225010cdf409d31915047666110ad745a2080dd
Ariel Santos <ariel.santos@dcc.ufmg.br>
2020-10-02 14:01:11 -0300

    Passando matrizes por referência

--- Matemática Discreta/relacao.c
+++ Matemática Discreta/relacao.c
@@ -18,14 +18,14 @@ typedef struct {
 } AdjacencyMatrix;
 
 // Verificadores
-bool checkReflexive(AdjacencyMatrix);
-bool checkSymmetric(AdjacencyMatrix);
-bool checkAntiSymmetric(AdjacencyMatrix matrix);
-void checkAsymmetric(AdjacencyMatrix);
-bool checkTransitive(AdjacencyMatrix);
-void checkEquivalence(bool, bool, bool);
-void checkPartialOrder(bool, bool, bool);
-void generateTransitiveClosure(AdjacencyMatrix);
+bool checkReflexive(const AdjacencyMatrix*);
+bool checkSymmetric(const AdjacencyMatrix*);
+bool checkAntiSymmetric(const AdjacencyMatrix*);
+bool checkAsymmetric(const AdjacencyMatrix*);
+bool checkTransitive(const AdjacencyMatrix*);
+bool checkEquivalence(bool, bool, bool);
+bool checkPartialOrder(bool, bool, bool);
+void generateTransitiveClosure(const AdjacencyMatrix*);
 
 // Input
 InputElements readElements();
@@ -33,9 +33,9 @@ AdjacencyMatrix readLinks(InputElements);
 
 // Utilidades
 AdjacencyMatrix initializeMatrix(m_size, m_size *);
-m_size findLabelIndex(AdjacencyMatrix, m_size);
+m_size findLabelIndex(const AdjacencyMatrix*, m_size);
 unsigned short index2D(m_size, m_size, m_size);
-void printPairs(AdjacencyMatrix);
+void printPairs(const AdjacencyMatrix*);
 void printResult(char *, bool);
 
 // Implementações
@@ -44,30 +44,30 @@ int main() {
   const AdjacencyMatrix matrix = readLinks(inputs);
   free(inputs.ids);
 
-  const bool reflexive = checkReflexive(matrix);
-  const bool symmetric = checkSymmetric(matrix);
-  const bool anti_symmetric = checkAntiSymmetric(matrix);
-  checkAsymmetric(matrix);
-  const bool transitive = checkTransitive(matrix);
+  const bool reflexive = checkReflexive(&matrix);
+  const bool symmetric = checkSymmetric(&matrix);
+  const bool anti_symmetric = checkAntiSymmetric(&matrix);
+  checkAsymmetric(&matrix);
+  const bool transitive = checkTransitive(&matrix);
   checkEquivalence(reflexive, symmetric, transitive);
   checkPartialOrder(reflexive, transitive, anti_symmetric);
-  generateTransitiveClosure(matrix);
+  generateTransitiveClosure(&matrix);
 
   free(matrix.links);
   free(matrix.index_map);
   return 0;
 }
 
-bool checkReflexive(AdjacencyMatrix matrix) {
+bool checkReflexive(const AdjacencyMatrix *matrix) {
   bool reflexive = true;
   bool irreflexive = true;
-  AdjacencyMatrix pairs_present = initializeMatrix(matrix.size, matrix.index_map),
-	  pairs_absent = initializeMatrix(matrix.size, matrix.index_map);
+  AdjacencyMatrix pairs_present = initializeMatrix(matrix->size, matrix->index_map),
+	  pairs_absent = initializeMatrix(matrix->size, matrix->index_map);
 
-  for (m_size i = 0; i < matrix.size; ++i) {
-	const unsigned short index = index2D(matrix.size, i, i);
+  for (m_size i = 0; i < matrix->size; ++i) {
+	const unsigned short index = index2D(matrix->size, i, i);
 
-	const bool pair_valid = matrix.links[index];
+	const bool pair_valid = matrix->links[index];
 
 	if (pair_valid) {
 	  irreflexive = false;
@@ -79,10 +79,10 @@ bool checkReflexive(AdjacencyMatrix matrix) {
   }
 
   printResult("Reflexiva", reflexive);
-  if (!reflexive) printPairs(pairs_absent);
+  if (!reflexive) printPairs(&pairs_absent);
 
   printResult("Irreflexiva", irreflexive);
-  if (!irreflexive) printPairs(pairs_present);
+  if (!irreflexive) printPairs(&pairs_present);
 
   free(pairs_present.links);
   free(pairs_absent.links);
@@ -90,19 +90,19 @@ bool checkReflexive(AdjacencyMatrix matrix) {
   return reflexive;
 }
 
-bool checkSymmetric(AdjacencyMatrix matrix) {
+bool checkSymmetric(const AdjacencyMatrix* matrix) {
   bool symmetric = true;
-  AdjacencyMatrix pairs_absent = initializeMatrix(matrix.size, matrix.index_map);
+  AdjacencyMatrix pairs_absent = initializeMatrix(matrix->size, matrix->index_map);
 
-  for (m_size i = 0; i < matrix.size; ++i) {
+  for (m_size i = 0; i < matrix->size; ++i) {
 	for (m_size k = 0; k < i; ++k) {
-	  const unsigned short index_top = index2D(matrix.size, i, k),
-		  index_bottom = index2D(matrix.size, k, i);
+	  const unsigned short index_top = index2D(matrix->size, i, k),
+		  index_bottom = index2D(matrix->size, k, i);
 
-	  if (matrix.links[index_top] == matrix.links[index_bottom]) continue;
+	  if (matrix->links[index_top] == matrix->links[index_bottom]) continue;
 
 	  symmetric = false;
-	  if (matrix.links[index_bottom])
+	  if (matrix->links[index_bottom])
 		pairs_absent.links[index_top] = true;
 	  else
 		pairs_absent.links[index_bottom] = true;
@@ -110,23 +110,23 @@ bool checkSymmetric(AdjacencyMatrix matrix) {
   }
 
   printResult("Simétrica", symmetric);
-  if (!symmetric) printPairs(pairs_absent);
+  if (!symmetric) printPairs(&pairs_absent);
 
   free(pairs_absent.links);
 
   return symmetric;
 }
 
-bool checkAntiSymmetric(AdjacencyMatrix matrix) {
+bool checkAntiSymmetric(const AdjacencyMatrix* matrix) {
   bool anti_symmetric = true;
-  AdjacencyMatrix pairs_present = initializeMatrix(matrix.size, matrix.index_map);
+  AdjacencyMatrix pairs_present = initializeMatrix(matrix->size, matrix->index_map);
 
-  for (m_size i = 0; i < matrix.size; ++i) {
+  for (m_size i = 0; i < matrix->size; ++i) {
 	for (m_size k = 0; k < i; ++k) {
-	  const unsigned short index_top = index2D(matrix.size, i, k),
-		  index_bottom = index2D(matrix.size, k, i);
+	  const unsigned short index_top = index2D(matrix->size, i, k),
+		  index_bottom = index2D(matrix->size, k, i);
 
-	  if (!(matrix.links[index_top] && matrix.links[index_bottom])) continue;
+	  if (!(matrix->links[index_top] && matrix->links[index_bottom])) continue;
 
 	  anti_symmetric = false;
 	  pairs_present.links[index_top] = true;
@@ -135,40 +135,42 @@ bool checkAntiSymmetric(AdjacencyMatrix matrix) {
   }
 
   printResult("Anti-simétrica", anti_symmetric);
-  if (!anti_symmetric) printPairs(pairs_present);
+  if (!anti_symmetric) printPairs(&pairs_present);
 
   free(pairs_present.links);
 
   return anti_symmetric;
 }
 
-void checkAsymmetric(AdjacencyMatrix matrix) {
+bool checkAsymmetric(const AdjacencyMatrix* matrix) {
   bool asymmetric = true;
 
-  for (m_size i = 0; i < matrix.size; ++i) {
-	for (m_size k = 0; k < matrix.size; ++k) {
-	  asymmetric &= matrix.links[index2D(matrix.size, i, k)] != matrix.links[index2D(matrix.size, k, i)];
+  for (m_size i = 0; i < matrix->size; ++i) {
+	for (m_size k = 0; k < matrix->size; ++k) {
+	  asymmetric &= matrix->links[index2D(matrix->size, i, k)] != matrix->links[index2D(matrix->size, k, i)];
 	}
   }
 
   printResult("Assimétrica", asymmetric);
+
+  return asymmetric;
 }
 
-bool checkTransitive(AdjacencyMatrix matrix) {
+bool checkTransitive(const AdjacencyMatrix* matrix) {
   bool transitive = true;
-  AdjacencyMatrix pairs_absent = initializeMatrix(matrix.size, matrix.index_map);
+  AdjacencyMatrix pairs_absent = initializeMatrix(matrix->size, matrix->index_map);
 
-  for (m_size i = 0; i < matrix.size; ++i) {
-	for (m_size k = 0; k < matrix.size; ++k) {
+  for (m_size i = 0; i < matrix->size; ++i) {
+	for (m_size k = 0; k < matrix->size; ++k) {
 	  if (i == k) continue;
 
-	  if (matrix.links[index2D(matrix.size, i, k)]) {
-		for (m_size j = 0; j < matrix.size; ++j) {
+	  if (matrix->links[index2D(matrix->size, i, k)]) {
+		for (m_size j = 0; j < matrix->size; ++j) {
 		  if (k == j) continue;
 
-		  const unsigned short index = index2D(matrix.size, i, j);
+		  const unsigned short index = index2D(matrix->size, i, j);
 
-		  if (matrix.links[index2D(matrix.size, k, j)] && !matrix.links[index]) {
+		  if (matrix->links[index2D(matrix->size, k, j)] && !matrix->links[index]) {
 			transitive = false;
 			pairs_absent.links[index] = true;
 		  }
@@ -178,24 +180,28 @@ bool checkTransitive(AdjacencyMatrix matrix) {
   }
 
   printResult("Transitiva", transitive);
-  if (!transitive) printPairs(pairs_absent);
+  if (!transitive) printPairs(&pairs_absent);
 
   free(pairs_absent.links);
 
   return transitive;
 }
 
-void checkEquivalence(bool reflexive, bool symmetric, bool transitive) {
-  printResult("Relação de equivalência", reflexive && symmetric && transitive);
+bool checkEquivalence(bool reflexive, bool symmetric, bool transitive) {
+  const bool equivalent = reflexive && symmetric && transitive;
+  printResult("Relação de equivalência", equivalent);
+  return equivalent;
 }
 
-void checkPartialOrder(bool reflexive, bool transitive, bool anti_symmetric) {
-  printResult("Relação de ordem parcial", reflexive && anti_symmetric && transitive);
+bool checkPartialOrder(bool reflexive, bool transitive, bool anti_symmetric) {
+  const bool partial_ordering = reflexive && anti_symmetric && transitive;
+  printResult("Relação de ordem parcial", partial_ordering);
+  return partial_ordering;
 }
 
-void generateTransitiveClosure(AdjacencyMatrix matrix) {
-  AdjacencyMatrix closure = initializeMatrix(matrix.size, matrix.index_map);
-  memcpy(closure.links, matrix.links, matrix.size * matrix.size * sizeof(bool));
+void generateTransitiveClosure(const AdjacencyMatrix* matrix) {
+  AdjacencyMatrix closure = initializeMatrix(matrix->size, matrix->index_map);
+  memcpy(closure.links, matrix->links, matrix->size * matrix->size * sizeof(bool));
 
   for (m_size i = 0; i < closure.size; ++i) {
 	for (m_size k = 0; k < closure.size; ++k) {
@@ -208,7 +214,7 @@ void generateTransitiveClosure(AdjacencyMatrix matrix) {
   }
 
   printf("Fecho transitivo da relação: ");
-  printPairs(closure);
+  printPairs(&closure);
 
   free(closure.links);
 }
@@ -234,8 +240,8 @@ AdjacencyMatrix readLinks(InputElements elements) {
   }
 
   while (scanf("%hhu %hhu", &x, &y) == 2) {
-	m_size x_index = findLabelIndex(matrix, x),
-		y_index = findLabelIndex(matrix, y);
+	m_size x_index = findLabelIndex(&matrix, x),
+		y_index = findLabelIndex(&matrix, y);
 	matrix.links[index2D(elements.size, x_index, y_index)] = true;
   }
 
@@ -252,9 +258,9 @@ AdjacencyMatrix initializeMatrix(m_size size, m_size *label_map) {
   return matrix;
 }
 
-m_size findLabelIndex(AdjacencyMatrix matrix, m_size label) {
-  for (m_size i = 0; i < matrix.size; ++i) {
-	if (matrix.index_map[i] == label) {
+m_size findLabelIndex(const AdjacencyMatrix* matrix, m_size label) {
+  for (m_size i = 0; i < matrix->size; ++i) {
+	if (matrix->index_map[i] == label) {
 	  return i;
 	}
   }
@@ -264,16 +270,16 @@ unsigned short index2D(m_size size, m_size line, m_size column) {
   return (line * size) + column;
 }
 
-void printPairs(AdjacencyMatrix matrix) {
+void printPairs(const AdjacencyMatrix* matrix) {
   bool first = true;
 
-  for (m_size i = 0; i < matrix.size; ++i) {
-	const m_size i_label = matrix.index_map[i];
+  for (m_size i = 0; i < matrix->size; ++i) {
+	const m_size i_label = matrix->index_map[i];
 
-	for (m_size k = 0; k < matrix.size; ++k) {
-	  const m_size k_label = matrix.index_map[k];
+	for (m_size k = 0; k < matrix->size; ++k) {
+	  const m_size k_label = matrix->index_map[k];
 
-	  if (matrix.links[index2D(matrix.size, i, k)]) {
+	  if (matrix->links[index2D(matrix->size, i, k)]) {
 		if (!first)
 		  putchar(' ');
 		printf("(%hhu,%hhu);", i_label, k_label);
