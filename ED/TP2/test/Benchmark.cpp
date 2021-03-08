9cf7f724813b4f21e4d89db0acddc7cb3eb7812f
Ariel Santos <ariel.santos@dcc.ufmg.br>
2021-03-08 06:50:47 -0300

    Benchmarck e complexidade assintótica

--- ED/TP2/test/Benchmark.cpp
+++ ED/TP2/test/Benchmark.cpp
@@ -1,11 +1,9 @@
 //
 // Created by Ariel Santos on 07/03/21.
 //
-#include <stdexcept>
 #include <fstream>
 #include <cassert>
-#include <iostream>
-
+#include <cstring>
 #include <benchmark/benchmark.h>
 
 #include "Array.hpp"
@@ -13,43 +11,36 @@
 
 class SortFixture : public benchmark::Fixture {
  public:
-  Array aleatorio = loadFile("dados_aleatorio.txt"),
-	  crescente = loadFile("dados_crescente.txt"),
-	  descrescente = loadFile("dados_descrescente.txt");
-  Array to_sort{0};
+  Array *to_sort{nullptr};
 
   void SetUp(::benchmark::State &state) override {
-	Array view(state.range(0));
-	Array *data;
+	std::string filename;
 	switch (state.range(1)) {
-	  case 0: data = &aleatorio;
+	  case 0: filename = "dados_aleatorio.txt";
 		break;
-	  case 1: data = &crescente;
+	  case 1: filename = "dados_crescente.txt";
 		break;
-	  case 2: data = &descrescente;
+	  case 2: filename = "dados_descrescente.txt";
 	}
-	std::copy(data->items, data->items + state.range(0), view.items);
-	to_sort = view;
-  }
-
-  void TearDown(::benchmark::State &st) override {}
- private:
-  static Array loadFile(const std::string &filename) {
-	Array data(200000);
+	to_sort = new Array(state.range(0));
 	std::ifstream file(filename);
-	for (size_t i = 0; i < data.size; ++i) {
+	for (size_t i = 0; i < state.range(0); ++i) {
 	  std::string name;
 	  distance_t distance;
 	  assert((file[i] >> name >> distance) && "Arquivo mal-formado");
-	  data[i] = BaseDistancia(name, distance);
+	  (*to_sort)[i] = BaseDistancia(name, distance);
 	}
-	return data;
+  }
+
+  void TearDown(::benchmark::State &st) override {
+	delete to_sort;
+	to_sort = nullptr;
   }
 };
 
 BENCHMARK_DEFINE_F(SortFixture, Insertion)(benchmark::State &state) {
   for (auto _ : state) {
-	sorting::insertion(to_sort);
+	sorting::insertion(*to_sort);
   }
 }
 BENCHMARK_REGISTER_F(SortFixture, Insertion)
@@ -57,7 +48,7 @@ BENCHMARK_REGISTER_F(SortFixture, Insertion)
 
 BENCHMARK_DEFINE_F(SortFixture, Merge)(benchmark::State &state) {
   for (auto _ : state) {
-	sorting::merge(to_sort);
+	sorting::merge(*to_sort);
   }
 }
 BENCHMARK_REGISTER_F(SortFixture, Merge)
@@ -65,7 +56,7 @@ BENCHMARK_REGISTER_F(SortFixture, Merge)
 
 BENCHMARK_DEFINE_F(SortFixture, Quick)(benchmark::State &state) {
   for (auto _ : state) {
-	sorting::quick(to_sort);
+	sorting::quick(*to_sort);
   }
 }
 BENCHMARK_REGISTER_F(SortFixture, Quick)
@@ -73,7 +64,7 @@ BENCHMARK_REGISTER_F(SortFixture, Quick)
 
 BENCHMARK_DEFINE_F(SortFixture, QuickMed3)(benchmark::State &state) {
   for (auto _ : state) {
-	sorting::quick_med3(to_sort);
+	sorting::quick_med3(*to_sort);
   }
 }
 BENCHMARK_REGISTER_F(SortFixture, QuickMed3)
@@ -81,10 +72,18 @@ BENCHMARK_REGISTER_F(SortFixture, QuickMed3)
 
 BENCHMARK_DEFINE_F(SortFixture, Cycle)(benchmark::State &state) {
   for (auto _ : state) {
-	sorting::cycle(to_sort);
+	sorting::cycle(*to_sort);
   }
 }
 BENCHMARK_REGISTER_F(SortFixture, Cycle)
-->ArgsProduct({{100, 500, 1000, 5000, 10000}, {0, 1, 2}});
+->ArgsProduct({{100, 500, 1000, 5000}, {0, 1, 2}});
+
+BENCHMARK_DEFINE_F(SortFixture, STD)(benchmark::State &state) {
+  for (auto _ : state) {
+	std::sort(to_sort->items, &to_sort->end());
+  }
+}
+BENCHMARK_REGISTER_F(SortFixture, STD)
+->ArgsProduct({{100, 500, 1000, 5000, 10000, 50000, 100000, 200000}, {0, 1, 2}});
 
 BENCHMARK_MAIN();
