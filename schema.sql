74314d8020b5a05069497b1599dfd3e8e0361dbc
Ariel Santos <ariel.santos@dcc.ufmg.br>
2021-03-18 00:48:09 -0300

    Script para importação dos dados e sqlite preparado

--- schema.sql
+++ schema.sql
@@ -1,73 +1,126 @@
-PRAGMA foreign_keys= ON;
+PRAGMA foreign_keys= OFF;
 
+
+DROP TABLE IF EXISTS orgao_superior;
 CREATE TABLE orgao_superior
 (
-    id INTEGER PRIMARY KEY
-);
+    codigo_sup INTEGER PRIMARY KEY,
+    nome       TEXT
+) WITHOUT ROWID;
 
+DROP TABLE IF EXISTS orgao_subordinado;
 CREATE TABLE orgao_subordinado
 (
-    id             INTEGER PRIMARY KEY,
-    orgao_superior INTEGER,
-    FOREIGN KEY (orgao_superior) references orgao_superior (id)
-);
+    codigo_sub INTEGER PRIMARY KEY,
+    nome       TEXT,
+    codigo_sup INTEGER,
+    FOREIGN KEY (codigo_sup) references orgao_superior (codigo_sup)
+) WITHOUT ROWID;
 
+DROP TABLE IF EXISTS unidade_gestora;
 CREATE TABLE unidade_gestora
 (
-    id INTEGER PRIMARY KEY
-);
+    codigo_gest INTEGER PRIMARY KEY,
+    nome        TEXT
+) WITHOUT ROWID;
 
+DROP TABLE IF EXISTS pessoa_juridica;
 CREATE TABLE pessoa_juridica
 (
-    id INTEGER PRIMARY KEY
-);
+    cnpj TEXT PRIMARY KEY,
+    nome TEXT
+) WITHOUT ROWID;
 
-CREATE TABLE compra
+-- DROP TABLE IF EXISTS compra;
+-- CREATE TABLE compra
+-- (
+--     id                INTEGER PRIMARY KEY,
+--     unidade_gestora   INTEGER,
+--     orgao_subordinado INTEGER,
+--     pessoa_juridica   INTEGER,
+--     FOREIGN KEY (unidade_gestora) references unidade_gestora (id),
+--     FOREIGN KEY (orgao_subordinado) references orgao_subordinado (id),
+--     FOREIGN KEY (pessoa_juridica) references pessoa_juridica (id)
+-- ) WITHOUT ROWID;
+--
+-- DROP TABLE IF EXISTS item_compra;
+-- CREATE TABLE item_compra
+-- (
+--     id     INTEGER PRIMARY KEY,
+--     compra INTEGER,
+--     FOREIGN KEY (compra) references compra (id)
+-- ) WITHOUT ROWID;
+--
+-- DROP TABLE IF EXISTS termo_aditivo;
+-- CREATE TABLE termo_aditivo
+-- (
+--     id     INTEGER PRIMARY KEY,
+--     compra INTEGER,
+--     FOREIGN KEY (compra) references compra (id)
+-- ) WITHOUT ROWID;
+--
+DROP TABLE IF EXISTS modalidade_compra;
+CREATE TABLE modalidade_compra
 (
-    id                INTEGER PRIMARY KEY,
-    unidade_gestora   INTEGER,
-    orgao_subordinado INTEGER,
-    pessoa_juridica   INTEGER,
-    FOREIGN KEY (unidade_gestora) references unidade_gestora (id),
-    FOREIGN KEY (orgao_subordinado) references orgao_subordinado (id),
-    FOREIGN KEY (pessoa_juridica) references pessoa_juridica (id)
-);
+    codigo_mod INTEGER PRIMARY KEY,
+    modalidade TEXT
+) WITHOUT ROWID;
 
-CREATE TABLE item_compra
-(
-    id     INTEGER PRIMARY KEY,
-    compra INTEGER,
-    FOREIGN KEY (compra) references compra (id)
-);
-
-CREATE TABLE termo_aditivo
-(
-    id     INTEGER PRIMARY KEY,
-    compra INTEGER,
-    FOREIGN KEY (compra) references compra (id)
-);
-
+DROP TABLE IF EXISTS licitacao;
 CREATE TABLE licitacao
 (
-    id                INTEGER PRIMARY KEY,
-    unidade_gestora   INTEGER,
-    orgao_subordinado INTEGER,
-    FOREIGN KEY (unidade_gestora) references unidade_gestora (id),
-    FOREIGN KEY (orgao_subordinado) references orgao_subordinado (id)
-);
+    numero         INTEGER,
+    codigo_mod     INTEGER,
+    processo       TEXT,
+    objeto         TEXT,
+    situacao       TEXT,
+    codigo_gest    INTEGER,
+    codigo_sub     INTEGER,
+    municipio      TEXT,
+    data_resultado TEXT,
+    data_abertura  TEXT,
+    valor          REAL,
 
+    PRIMARY KEY (numero, objeto),
+    FOREIGN KEY (codigo_mod) references modalidade_compra (codigo_mod),
+    FOREIGN KEY (codigo_gest) references unidade_gestora (codigo_gest),
+    FOREIGN KEY (codigo_sub) references orgao_subordinado (codigo_sub)
+) WITHOUT ROWID;
+
+DROP TABLE IF EXISTS item_licitacao;
 CREATE TABLE item_licitacao
 (
-    id        INTEGER PRIMARY KEY,
-    licitacao INTEGER,
-    FOREIGN KEY (licitacao) references licitacao (id)
+    numero          INTEGER,
+    codigo_mod      INTEGER,
+    processo        TEXT,
+    codigo_gest     INTEGER,
+    codigo_sub      INTEGER,
+    descricao       TEXT,
+    vencedor_cnpj   TEXT,
+    quantidade_item INTEGER,
+    valor_item      REAL,
+
+    FOREIGN KEY (codigo_mod) references modalidade_compra (codigo_mod),
+    FOREIGN KEY (codigo_gest) references unidade_gestora (codigo_gest),
+    FOREIGN KEY (codigo_sub) references orgao_subordinado (codigo_sub),
+    FOREIGN KEY (vencedor_cnpj) references pessoa_juridica (cnpj)
 );
 
+DROP TABLE IF EXISTS participante_licitacao;
 CREATE TABLE participante_licitacao
 (
-    id              INTEGER PRIMARY KEY,
-    item_licitacao  INTEGER,
-    pessoa_juridica INTEGER,
-    FOREIGN KEY (item_licitacao) references item_licitacao (id),
-    FOREIGN KEY (pessoa_juridica) references pessoa_juridica (id)
+    numero      INTEGER,
+    codigo_mod  INTEGER,
+    processo    TEXT,
+    codigo_gest INTEGER,
+    codigo_sub  INTEGER,
+    descricao   TEXT,
+    cnpj        TEXT,
+    vencedor    INTEGER,
+
+    FOREIGN KEY (codigo_gest) references unidade_gestora (codigo_gest),
+    FOREIGN KEY (codigo_mod) references modalidade_compra (codigo_mod),
+    FOREIGN KEY (codigo_sub) references orgao_subordinado (codigo_sub),
+    FOREIGN KEY (cnpj) references pessoa_juridica (cnpj)
 );
+PRAGMA foreign_keys= ON;
